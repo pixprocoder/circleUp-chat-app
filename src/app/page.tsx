8bb0d019f3f0881df23ecd4e7cdeb5201a5a452e
samsul kobir <makkobir79@gmail.com>
2024-02-12 23:55:16 +0200

    redis database setup successfully

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  await db.set("hello", "hello");
   return (
     <>
       <Button>Hello</Button>
