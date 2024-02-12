52996a4c2724092c9cc5bd84a0cdcfeaedee487c
samsul kobir <makkobir79@gmail.com>
2024-02-13 00:26:30 +0200

    loading added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
 
 const LoginPage = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  async function loginWithGoogle() {
+    setIsLoading(true);
+    try {
+      console.log("object");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div>
-      <h1 className="text-center">Login</h1>
+      <h1 className="text-center text-2xl">Login</h1>
+      <Button onClick={loginWithGoogle}>
+        {isLoading ? "Loading..." : "Continue with google"}
+      </Button>
     </div>
   );
 };
